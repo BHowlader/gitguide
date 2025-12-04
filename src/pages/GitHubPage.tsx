2d968cb0343f82e082ba93a050885d122ad27cd5
BHowlader <bibekhowlader8@gmail.com>
2025-12-05 02:41:57 +0600

    🎨 Layout: Remove excessive whitespace above footer on pages

--- src/pages/GitHubPage.tsx
+++ src/pages/GitHubPage.tsx
@@ -3,7 +3,7 @@ import GitHub from '../components/GitHub';
 
 const GitHubPage = () => {
     return (
-        <div className="pt-20 min-h-screen animate-fade-in">
+        <div className="pt-20 animate-fade-in">
             <GitHub />
         </div>
     );
--- src/pages/QuickStartPage.tsx
+++ src/pages/QuickStartPage.tsx
@@ -3,7 +3,7 @@ import QuickStart from '../components/QuickStart';
 
 const QuickStartPage = () => {
     return (
-        <div className="pt-20 min-h-screen animate-fade-in">
+        <div className="pt-20 animate-fade-in">
             <QuickStart />
         </div>
     );
